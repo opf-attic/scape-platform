92277f7c28ca53f58c5a67844b8149803237730a
Matthias Rella <rella@myrho.net>
2014-01-23 10:19:38 +0100

    slight cosmetic changes

--- metshadoop/src/main/java/eu/scape_project/pt/mets/hadoop/dummy/DummyJobMapper.java
+++ metshadoop/src/main/java/eu/scape_project/pt/mets/hadoop/dummy/DummyJobMapper.java
@@ -1,7 +1,9 @@
 package eu.scape_project.pt.mets.hadoop.dummy;
 
 import eu.scape_project.pt.mets.hadoop.DTO;
+import eu.scapeproject.model.IntellectualEntity;
 import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.slf4j.Logger;
@@ -17,13 +19,20 @@ public class DummyJobMapper extends Mapper<Text, DTO, Text, DTO> {
 
     @Override
     public void setup(Context context) {
-        DTO.setType( String.class );
+	Configuration conf = context.getConfiguration();
+	String type = conf.get("DTO.type");
+	try{
+	    DTO.setType( Class.forName(type) );
+	} catch(ClassNotFoundException ex ){
+ 	    LOG.error(ex.getMessage());
+	}
     }
 
     @Override
     public void map(Text key, DTO value, Context context)
             throws IOException, InterruptedException {
-        LOG.debug( "DTO is landed: id = " + value.getIdentifier() );
+
+        System.out.println( "DTO is landed: id = " + value.getIdentifier() );
 
         context.write(key, value);
 
